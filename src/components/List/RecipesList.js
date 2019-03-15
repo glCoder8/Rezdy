289e9a6869fe3ce5b24b45a2521750a35065e4d6
Gleb Mikheenko <glebmikheenk0o@gmail.com>
2019-03-15 11:03:03 +0300

    - add ingredient list

--- src/components/List/RecipesList.js
+++ src/components/List/RecipesList.js
@@ -1,12 +1,36 @@
 import React from 'react';
 
 const styles = {
+  recipeContainer: {
+    backgroundColor: '#bbcfa7',
+    borderWidth: 1,
+    borderColor: '#bbbbbb',
+    borderStyle: 'solid',
+    marginBottom: 10,
+    padding: 10
+  },
+  recipe: {
+    fontWeight: 'bold'
+  },
+  ingredient: {
+
+  }
 };
 
 export default ({ data }) => (
   <div>
     { data.map((value, index) => {
-      return <div key={index}>{value.title}</div>
+      return (
+        <div style={styles.recipeContainer} key={index}>
+          <div style={styles.recipe} key={index}>{value.title}</div>
+          <p>What you will need</p>
+          <ul>
+            { value.ingredients.map((ingredient, ing_index) => {
+              return <li style={styles.ingredient} key={ing_index}>{ingredient}</li>
+            })}
+          </ul>
+        </div>
+      )
     })}
   </div>
 )
